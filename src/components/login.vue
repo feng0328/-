aef673a566d5209742cf06ef65256f9e87eae056
ljf <296348573@qq.com>
2019-02-15 16:33:49 +0800

    登录-表单组件引

---
+++ src/components/login.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="login-wrap">
+      <h2>用户登录</h2>
+    <el-form
+    class="login-form"
+    label-position="top" label-width="80px" :model="formdata">
+      <el-form-item label="用户名">
+        <el-input v-model="formdata.username"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input v-model="formdata.password"></el-input>
+      </el-form-item>
+    </el-form>
+    <el-button class="login-btn" type="primary" disabled>登录</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      formdata: {
+        username: '',
+        password: ''
+      }
+    }
+  }
+}
+</script>
+
+<style>
+</style>
