d07cef575703125b5f645bf872c678bd46593071
ljf <296348573@qq.com>
2019-02-15 17:38:29 +0800

    登录-发送请

--- src/main.js
+++ src/main.js
@@ -5,6 +5,10 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import App from './App'
 import router from './router'
+import './assets/css/base.css'
+import axios from 'axios'
+axios.defaults.baseURL = 'http://localhost:8888/api/private/v1/'
+Vue.prototype.$http = axios
 // vue插件的使用
 Vue.use(ElementUI)
 
