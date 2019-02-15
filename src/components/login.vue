f9324748616d874d48ede8be3718d8103b578015
ljf <296348573@qq.com>
2019-02-15 19:51:13 +0800

    主页-分布组件完成

--- src/components/login.vue
+++ src/components/login.vue
@@ -15,18 +15,18 @@
 
 <script>
 export default {
-  data() {
+  data () {
     return {
       formdata: {
-        username: "",
-        password: ""
+        username: '',
+        password: ''
       }
-    };
+    }
   },
   methods: {
     // 发起登录请求
-    async handlelogin() {
-      const res = await this.$http.post(`login`, this.formdata);
+    async handlelogin () {
+      const res = await this.$http.post(`login`, this.formdata)
       const {
         data: {
           data: {token},
@@ -35,18 +35,18 @@ export default {
       } = res
       //
       if (status === 200) {
-        console.log("login---成功")
-        localStorage.setItem('token',token)
+        console.log('login---成功')
+        localStorage.setItem('token', token)
 
         this.$router.push({
-          name: "home"
+          name: 'home'
         })
       } else {
         this.$message.error(msg)
       }
     }
   }
-};
+}
 </script>
 
 <style>
